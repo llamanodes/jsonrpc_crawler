6ec9f56d6063bb2be742855955e1d3a16a46682b
Bryan Stitt <bryan@stitthappens.com>
2023-10-29 11:44:45 -0700

    need meta when following links

--- jsonrpc_crawler/spiders/jsonrpc_spider.py
+++ jsonrpc_crawler/spiders/jsonrpc_spider.py
@@ -1,7 +1,12 @@
-from playwright.async_api import Request as PlaywrightRequest, Response as PlaywrightResponse
+from playwright.async_api import (
+    Request as PlaywrightRequest,
+    Response as PlaywrightResponse,
+)
 import scrapy
+import json
 
 # from scrapy_playwright.page import PageCoroutine
+
 
 class JsonrpcSpider(scrapy.Spider):
     name = "jsonrpc"
@@ -14,17 +19,18 @@ class JsonrpcSpider(scrapy.Spider):
             "https": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
         },
     }
+    handled = set()
 
     def scrapy_meta(self, **kwargs):
         meta = dict(
-            errback = self.errback,
-            playwright = True,
-            playwright_include_page = True,
-            playwright_page_event_handlers = {
+            errback=self.errback,
+            playwright=True,
+            playwright_include_page=True,
+            playwright_page_event_handlers={
                 "request": "handle_request",
-                #"response": "handle_response",  # TODO: do we want this?
+                # "response": "handle_response",  # TODO: do we want this?
             },
-            playwright_page_coroutines = [
+            playwright_page_coroutines=[
                 # TODO: what should we do? inject some javascript to connect a mock wallet/provider?
                 # PageCoroutine("wait_for_timeout", 30 * 1000)
             ],
@@ -36,42 +42,74 @@ class JsonrpcSpider(scrapy.Spider):
 
     def start_requests(self):
         yield scrapy.Request(
-            "https://curve.fi",
-            meta=self.scrapy_meta(playwright_context= "curve")
-        )
-        yield scrapy.Request(
-            "https://dao.curve.fi",
-            meta=self.scrapy_meta(playwright_context= "curve dao")
+            "https://curve.fi", meta=self.scrapy_meta(playwright_context="curve")
         )
         yield scrapy.Request(
             "https://www.convexfinance.com",
-            meta=self.scrapy_meta(playwright_context= "convex")
+            meta=self.scrapy_meta(playwright_context="convex"),
         )
 
     async def parse(self, response: PlaywrightResponse):
         # 'response' contains the page as seen by the browser
 
+        # TODO: skip scraping non-html pages
+        # if response.content_type != "text/html":
+        #     return
+
+        # playwright_page is set when request.meta.playwright == True
         page = response.meta["playwright_page"]
 
-        return {"url": response.url, "page": page}
+        # TODO: if page is set, wait for the page to load? (it isn't always set. why?)
+
+        try:
+            # TODO: does `response` work here or do we need to parse the `page`
+            links = response.xpath("//a[@href]/@href").getall()
+        except Exception:
+            # TODO: just the one type of exception
+            return
+
+        yield {"url": response.url, "page": page}
+
+        for link in links:
+            if link in self.handled:
+                continue
+
+            if link.endswith(".js") or link.endswith(".png"):
+                continue
+
+            self.handled.add(link)
+
+            # TODO: do some extra filtering on these links? i think scrapy handles enough for us with self.allowed_domains
+            self.logger.debug("following link on %s: %s", response.request.url, link)
+
+            follow_meta = self.scrapy_meta(
+                playwright_context=response.meta["playwright_context"]
+            )
+
+            yield response.follow(link, callback=self.parse, meta=follow_meta)
 
     async def handle_request(self, request: PlaywrightRequest) -> None:
         # self.logger.debug(
         #     f"request seen: {request.resource_type, request.url, request.method}"
         # )
 
-        if request.resource_type == "fetch" and request.method == "POST":
-            self.logger.info("fetch POST request")
-
-            data = request.post_data_json
+        if request.method == "POST" and (
+            request.resource_type == "fetch" or request.resource_type == "xhr"
+        ):
+            try:
+                data = request.post_data_json
+            except Exception:
+                # TODO: just the one type of exception
+                return
 
             if data is None:
                 return
 
             if "jsonrpc" in data:
-                print(data)
+                # TODO: include request.url in this so that we can tell what rpc/chain it is going to
+                # TODO: should this yield an `Item`?
+                print(json.dumps(dict(url=request.url, data=data)))
 
     async def errback(self, failure):
         page = failure.request.meta["playwright_page"]
         await page.close()
- 
