95514031c2c7a2ac5c5ff247b13c019b2e607db7
Bryan Stitt <bryan@stitthappens.com>
2023-10-29 17:04:15 -0700

    preload console logging works

--- jsonrpc_crawler/spiders/jsonrpc_spider.py
+++ jsonrpc_crawler/spiders/jsonrpc_spider.py
@@ -25,20 +25,22 @@ class JsonrpcSpider(scrapy.Spider):
         meta = dict(
             # TODO: we probably need to include_page so that we can click all the things that aren't actually links
             # playwright_include_page=False,
-
             playwright_include_page=True,
             errback=self.errback,
-
             playwright=True,
             playwright_page_event_handlers={
+                "console": "handle_console",
                 "request": "handle_request",
             },
+            playwright_page_init_callback=self.init_callback,
             playwright_page_methods=[
-                # attach our crawler wallet and provider
-                PageMethod('add_init_script', path='preload.js'),
+                # # attach our crawler wallet and provider
+                # PageMethod('add_init_script', path='preload.js'),
                 # TODO: if there is a "connect wallet" button on the page, click it
                 # TODO: timeout on this is probably a good idea. it seems to throw an exception instead of just exiting quietly. we want a quiet exit
-                PageMethod('wait_for_load_state', state='networkidle', timeout=30 * 1000),
+                PageMethod(
+                    "wait_for_load_state", state="networkidle", timeout=30 * 1000
+                ),
             ],
         )
 
@@ -46,51 +48,53 @@ class JsonrpcSpider(scrapy.Spider):
 
         return meta
 
+    async def init_callback(self, page, request):
+        await page.add_init_script(path="./preload.js")
+
     def start_requests(self):
         yield scrapy.Request(
-            "https://curve.fi/", meta=self.scrapy_meta(playwright_context="curve")
+            "https://curve.fi/", meta=self.scrapy_meta()
         )
-        yield scrapy.Request(
-            "https://classic.curve.fi/", meta=self.scrapy_meta(playwright_context="curve classic")
-        )
-        yield scrapy.Request(
-            "https://curve.fi/#/ethereum/pools/3pool/deposit", meta=self.scrapy_meta(playwright_context="curve 3pool")
-        )
-        yield scrapy.Request(
-            "https://www.convexfinance.com",
-            meta=self.scrapy_meta(playwright_context="convex"),
-        )
+        # yield scrapy.Request(
+        #     "https://classic.curve.fi/", meta=self.scrapy_meta()
+        # )
+        # yield scrapy.Request(
+        #     "https://curve.fi/#/ethereum/pools/3pool/deposit", meta=self.scrapy_meta()
+        # )
+        # yield scrapy.Request(
+        #     "https://www.convexfinance.com",
+        #     meta=self.scrapy_meta(),
+        # )
 
     async def parse(self, response: HtmlResponse):
         page = response.meta["playwright_page"]
 
         # TODO: gather all the things that aren't actually links but still point to other pages
 
-        await page.close()
-
         if response.status != 200:
+            await page.close()
             return
 
-        if not response.headers[b'Content-Type'].startswith(b"text/html"):
+        if not response.headers[b"Content-Type"].startswith(b"text/html"):
             self.logger.debug("skipping non html: %s", response.url)
+            await page.close()
             return
 
         try:
             # TODO: does `response` work here or do we need to parse the `page`
-            links = response.xpath("//a[@href]/@href").getall()
+            # links = response.xpath("//a[@href]/@href").getall()
+            links = []
         except Exception:
             # TODO: just the one type of exception
             links = []
 
         # TODO: `screenshot = await page.screenshot(path="example.png", full_page=True)`
 
-        return
-
         for link in links:
             if link in self.handled:
                 continue
 
-            # TODO: do this less fragile
+            # TODO: do this less fragile. can we re-use the allowed_domains check?
             # TODO: skip images, css, js, xml, etc
             if "gov.curve.fi" in link or "api.curve.fi" in link or "github.com" in link:
                 continue
@@ -101,15 +105,24 @@ class JsonrpcSpider(scrapy.Spider):
             self.logger.debug("following link on %s: %s", response.request.url, link)
 
             follow_meta = self.scrapy_meta(
-                playwright_context=response.meta["playwright_context"]
+                playwright_page=page,
             )
 
             # TODO: follow links once loading one page works fully
             # TODO: following links should be optional
             yield response.follow(link, callback=self.parse, meta=follow_meta)
 
+        await page.close()
+
+    async def handle_console(self, msg):
+        self.logger.info("console.log: %s", msg)
+
     def handle_request(self, request: PlaywrightRequest) -> None:
-        if request.method == "POST" and request.resource_type in ["fetch", "xhr"] and "jsonrpc" in request.post_data:
+        if (
+            request.method == "POST"
+            and request.resource_type in ["fetch", "xhr"]
+            and "jsonrpc" in request.post_data
+        ):
             try:
                 request_json = request.post_data_json
             except Exception:
